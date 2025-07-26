0872a812d19334b26d3ef7353320862411dec799
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-07-26 18:20:31 +0300

    modified the classname of remove button

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener(DOMContentLoaded, function () {
 
       const removeButton = document.createElement("button");
       removeButton.textContent = "Remove";
-      removeButton.className = "remove-btn";
+      removeButton.classList.add("remove-btn");
 
       //add Event
       removeButton.onclick = () => {
