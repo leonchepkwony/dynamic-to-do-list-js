047989cb0736a0cd450caba86a0cd7c41d233219
Leonard Chepkwony <leonchepkwony@gmail.com>
2025-07-26 16:38:39 +0300

    Initial Commit.

---
+++ script.js
@@ -0,0 +1,37 @@
+const addButton = document.getElementById("add-task-btn");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task.");
+    return;
+  }
+  // new list element
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.className = "remove-btn";
+
+  //add Event
+  removeButton.onclick = () => {
+    taskList.removeChild(li);
+  };
+
+  //Appending the button
+  li.appendChild(removeButton);
+  taskList.appendChild(li);
+
+  // clear the input field
+  taskInput.value = "";
+}
+addButton.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
